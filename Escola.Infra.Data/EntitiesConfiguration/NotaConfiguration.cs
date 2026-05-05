da36749187ee5248f79baf0408909a5ced425f78
Kevin Lopes Costa <kevinlopes3012@gmail.com>
2026-05-05 16:14:22 -0300

    feat: entities configuration

---
+++ Escola.Infra.Data/EntitiesConfiguration/NotaConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Escola.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Escola.Infra.Data.EntitiesConfiguration
+{
+    public class NotaConfiguration : IEntityTypeConfiguration<Nota>
+    {
+        public void Configure(EntityTypeBuilder<Nota> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.ValorNota)
+                .IsRequired();
+            builder.Property(m => m.MatriculaId)
+                .IsRequired();
+
+            builder.HasOne(m => m.Matricula)
+                .WithMany(m => m.Notas)
+                .HasForeignKey(m => m.MatriculaId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ Escola.Infra.Data/EntitiesConfiguration/TurmaConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Escola.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Escola.Infra.Data.EntitiesConfiguration
+{
+    public class TurmaConfiguration : IEntityTypeConfiguration<Turma>
+    {
+        public void Configure(EntityTypeBuilder<Turma> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Nome)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(m => m.Descricao)
+                .IsRequired()
+                .HasMaxLength(150);
+            builder.Property(m => m.CursoId)
+                .IsRequired();
+
+            builder.HasOne(t => t.Curso)
+                .WithMany(c => c.Turmas)
+                .HasForeignKey(t => t.CursoId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+        }
+    }
+}
